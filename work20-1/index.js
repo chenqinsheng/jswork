3ab1f54382808349fe3dce424c69eb9a7129c28f
chenqinsheng <2537522174@qq.com>
2019-12-29 17:56:09 +0800

    work20

--- work20-1/index.js
+++ work20-1/index.js
@@ -1,13 +1,12 @@
-//
-$('#toRight').click(function () { //右移
+$('#toRight').click(function () {
     $('#right>select').append($('#left>select>option:selected'));
 });
-$('#toRight').click(function () { //左移
-    $('#left>select').append($('#left>select>option:selected'));
+$('#toLeft').click(function () {
+    $('#left>select').append($('#right>select>option:selected'));
 });
-$('#toAllRight').click(function () { //全部右移
+$('#toAllRight').click(function () {
     $('#right>select').append($('#left>select>option'));
 });
-$('#toAllLeft').click(function () { //全部左移
+$('#toAllLedt').click(function () {
     $('#left>select').append($('#right>select>option'));
 });
